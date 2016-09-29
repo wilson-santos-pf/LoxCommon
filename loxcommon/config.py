8137b04df42f9ec99015acb8d985a2b1470af4c2
wilson-santos-pf <wilson.santos@penguinformula.com>
2016-09-29 16:43:03 +0100

    added config

---
+++ loxcommon/config.py
@@ -0,0 +1,94 @@
+"""
+Module holding configuration and configparser related functions
+"""
+
+from logging import getLogger
+from logging import StreamHandler
+import os
+
+try:
+    from ConfigParser import ConfigParser
+    from ConfigParser import NoOptionError, NoSectionError
+except ImportError:
+    from configparser import ConfigParser  # pylint: disable=F0401
+    # pylint: disable=F0401
+    from configparser import NoOptionError, NoSectionError
+
+
+def prepare_logger(name, loglevel=None, handlers=None):
+    """
+    Make sure the logging subsystem is initialized correctly
+    """
+    if handlers is None:
+        handlers = [StreamHandler()]
+    log = getLogger(name)
+    log.setLevel(loglevel)
+    for handler in handlers:
+        log.addHandler(handler)
+
+
+class ConfigSingleton(object):
+    """
+    Singleton which has all configruation related info.
+    """
+    _instance = None
+
+    def __new__(cls, *args, **kwargs):
+        if not cls._instance:
+            cls._instance = super(ConfigSingleton, cls).__new__(
+                cls, *args, **kwargs)
+        return cls._instance
+
+    def __init__(self, module=None, location=None):
+        if not hasattr(self, 'configparser'):
+            self.configparser = ConfigParser()
+            if not location:
+                self.configparser.read(['/etc/%s.ini' % module, os.path.expanduser('~/%s.ini' % module),
+                                        os.path.expanduser('~/.config/%s/config.ini' % module),
+                                        '%s.ini' % module])
+            else:
+                self.configparser.read(location)
+
+    def get(self, section, field, default=None):
+        """
+        Returns the value of a certain field in a certain section on the
+        configuration
+        @param section the [section] in which to look for the information
+        @param field the name of the configuration item to read
+        @param default Value to return when the config option can't be found
+        @return the value of said configuration item
+        """
+        try:
+            result = self.configparser.get(section, field)
+        except (NoOptionError, NoSectionError):
+            result = default
+        return result
+
+    def getboolean(self, section, field, default=None):
+        """
+        Returns the value of a certain field in a certain section on the
+        configuration in a boolean context
+        @param section the [section] in which to look for the information
+        @param field the name of the configuration item to read
+        @param default Value to return when the config option can't be found
+        @return the boolean value of said configuration item
+        """
+        try:
+            result = self.configparser.getboolean(section, field)
+        except (NoOptionError, NoSectionError):
+            result = default
+        return result
+
+    def getint(self, section, field, default=None):
+        """
+        Returns the value of a certain field in a certain section on the
+        configuration, cast to an int.
+        @param section the [section] in which to look for the information
+        @param field the name of the configuration item to read
+        @return the integer value of said configuration item
+        """
+        try:
+            result = self.configparser.getint(section, field)
+        except (NoOptionError, NoSectionError):
+            result = default
+        return result
